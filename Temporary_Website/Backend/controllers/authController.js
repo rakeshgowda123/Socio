8bb61034e41d5c66b27cac2424dbca8308d50ba9
Rakeshgowda <130374347+rakeshgowda123@users.noreply.github.com>
2024-11-03 00:29:59 +0530

    Update authController.js

--- Temporary_Website/Backend/controllers/authController.js
+++ Temporary_Website/Backend/controllers/authController.js
@@ -4,8 +4,6 @@ const UserModel = require("../modules/user");
 
 const imgurl = "../";
 
-const sendEmail = require("../SendingBlue");
-const sendWhatsApp = require("../whastapp");
 
 const signup = async (req, res) => {
   try {
