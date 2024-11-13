6c5e23803500112b5e2edb3e90b2cc75e7dcd83f
Rakeshgowda <130374347+rakeshgowda123@users.noreply.github.com>
2024-11-13 12:06:06 +0530

    Update util.js

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -3,13 +3,13 @@ import { toast, ToastContainer } from "react-toastify";
 export const handleSuccess=(msg)=>{
     toast.success(msg,{
         position:'top-left',
-        autoClose:1000
+        autoClose:2000
     })
 }
 
 export const handleError=(msg)=>{
     toast.error(msg,{
         position:'top-left',
-         autoClose:1000
+         autoClose:2000
     })
 }
